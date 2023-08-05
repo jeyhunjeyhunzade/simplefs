f1ac1521e970c6f8d568b20010cf146dec14c144
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-06 02:54:52 +0300

    fixed react table type problem & added initial table

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,9 +1,113 @@
-const App = () => {
+/* eslint-disable react/jsx-key */
+import { useMemo } from "react";
+import {
+  useGlobalFilter,
+  usePagination,
+  useRowSelect,
+  useTable,
+} from "react-table";
+import { useRowSelectColumn } from "@lineup-lite/hooks";
+
+import { data } from "../data/dummy";
+
+import StatusPill from "./StatusPill";
+
+const OverviewTable = () => {
+  const columns: any = useMemo(
+    () => [
+      {
+        Header: "User Id",
+        accessor: "userId",
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Last Login",
+        accessor: "lastLoginTime",
+      },
+      {
+        Header: "Registeration time",
+        accessor: "registrationTime",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+        Cell: StatusPill,
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, prepareRow, page } =
+    useTable(
+      { columns, data },
+      useGlobalFilter,
+      usePagination,
+      useRowSelect,
+      useRowSelectColumn
+    );
+
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      SALAM
+    <div className="flex h-full items-center justify-center">
+      <div className="mt-2 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+              <table
+                {...getTableProps()}
+                className="min-w-full divide-y divide-gray-200"
+              >
+                <thead className="bg-gray-10">
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <th
+                          {...column.getHeaderProps()}
+                          className="text-20 rounded-sm px-6 py-5 font-medium uppercase tracking-wider text-gray-400"
+                        >
+                          {column.render("Header")}
+                          {column.id === "selection" &&
+                            column.render("Summary")}
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody
+                  {...getTableBodyProps()}
+                  className="divide-y divide-gray-200 bg-white"
+                >
+                  {page.map((row) => {
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => {
+                          return (
+                            <td
+                              {...cell.getCellProps()}
+                              className="whitespace-nowrap px-6 py-10"
+                            >
+                              {cell.render("Cell")}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default App;
+export default OverviewTable;
