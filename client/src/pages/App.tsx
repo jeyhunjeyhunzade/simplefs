d4460b1c78488aa6f0b161f7147ac76540f09e93
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-06 02:05:27 +0300

    added login and signup pages

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,3 +1,9 @@
-export default function App() {
-  return <>SALAM</>;
-}
+const App = () => {
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      SALAM
+    </div>
+  );
+};
+
+export default App;
