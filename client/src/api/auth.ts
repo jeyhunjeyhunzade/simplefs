8103a9ca71a8f985d130d3ee3f5e34b1cd1e4055
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-06 22:07:12 +0300

    fixed comparing hash password with plain one

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -7,7 +7,18 @@ interface LoginData {
   password: string;
 }
 
+interface SignUpData {
+  email: string;
+  password: string;
+  name: string;
+}
+
 export const loginAccount = async (loginData: LoginData) => {
   const res = await axios.post(`${localserver}/login`, loginData);
+  return res?.data;
+};
+
+export const createAccount = async (signUpData: SignUpData) => {
+  const res = await axios.post(`${localserver}/signUp`, signUpData);
   return res?.data;
 };
