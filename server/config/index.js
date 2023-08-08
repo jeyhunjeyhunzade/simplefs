43446a114e358db20aade1f3b4eb66ecf1fa04c7
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-09 01:37:48 +0300

    initialized db configs

--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 
 const Pool = require("pg").Pool;
-const pool = new Pool({
-  database: "postgres",
-  user: "postgres",
-  password: process.env.DBPASSWORD,
-  port: 5432,
-  host: "localhost",
-});
 
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.DBPASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction
+    ? process.env.DATABASE_URL //
+    : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
 module.exports = pool;
