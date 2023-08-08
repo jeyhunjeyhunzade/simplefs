17e0673c5343f7f22a408b6544f5ac951d148fb0
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-09 02:03:43 +0300

    changed heroku db var

--- server/config/index.js
+++ server/config/index.js
@@ -6,7 +6,9 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.DBPASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: isProduction
+    ? process.env.HEROKU_POSTGRESQL_CYAN_URL
+    : connectionString,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 module.exports = pool;
